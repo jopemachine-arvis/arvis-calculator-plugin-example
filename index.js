6df603489689fd60171f65382ead4a72ebb62883
jopemachine <jopemachine@naver.com>
2021-05-15 20:51:46 +0900

    Upd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const clipboardy = require("clipboardy");
 
 const searchPluginCommand = (inputStr) => {
+  // Return plugin's result in the form of 'items'
   let items = [];
   try {
     const result = eval(inputStr);
@@ -8,14 +9,16 @@ const searchPluginCommand = (inputStr) => {
       items.push({
         title: result,
         subtitle: `${inputStr} = ${result}`,
-        bundleId: "com.jopemachine.arvis-calculator-plugin",
+        bundleId: "com.jopemachine.arvis-calculator-plugin-example",
+        // Callback event when PluginOutputItem is clicked
         actionCallback: () => {
           clipboardy.write(result);
         },
       });
     }
   } catch (err) {
-    // If inputStr is not proper numerical expression, catchs err
+    // If inputStr is not proper numerical expression, catchs err.
+    // Return an empty array to 'items' if you do not need to include this plugin's items.
   }
 
   return {
@@ -23,4 +26,5 @@ const searchPluginCommand = (inputStr) => {
   };
 };
 
+// Export a function that has inputStr as a argument.
 module.exports = searchPluginCommand;
